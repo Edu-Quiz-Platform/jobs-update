ca594991f38100e75e8123758bfd5eda625baebf
raja <rashidaliwassan786@gmail.com>
2024-12-21 12:00:39 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <p>&copy; 2024 Your Platform Name. All Rights Reserved.</p>
+    <nav>
+        <a href="about.php">Raja Jani</a>
+        <a href="contact.php">Contact</a>
+        <a href="privacy.php">Privacy Policy</a>
+    </nav>
+</footer>
+
